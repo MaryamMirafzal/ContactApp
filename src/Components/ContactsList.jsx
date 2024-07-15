b3eeae2600717081d2417cbbba8fe6e55a820c8d
Maryam Mirafzal <maryammirafzal9@gmail.com>
2024-07-15 20:59:43 +0330

    Render Contacts

--- src/Components/ContactsList.jsx
+++ src/Components/ContactsList.jsx
@@ -3,10 +3,25 @@ function ContactsList({contacts}) {
   return (
     <div>
       <h3>Contacts List</h3>
-      <ul>
+      {contacts.length?
+        <ul>
         {contacts.map((contact) =>
-          (<li key={contact.id}>{contact.name}</li>))}
-      </ul>
+          (<li key={contact.id}>
+            <p>{contact.name} {contact.lastName}</p>
+            <p>
+              <span>📬</span>
+              {contact.email}
+            </p>
+            <p>
+              <span>📞</span>
+              {contact.number}
+            </p>
+              <button>🗑</button>
+          </li>))}
+        </ul>:
+      <p>No Contacts yet!</p>
+      }
+      
     </div>
   )
 }
